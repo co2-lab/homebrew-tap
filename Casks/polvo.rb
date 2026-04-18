22c00e30c1284d00934b0f03e4eb5d11184624f7
Adriel Codeco <codeco@outlook.com>
2026-04-18 13:40:08 +0000

    Update polvo to v0.3.1

--- Casks/polvo.rb
+++ Casks/polvo.rb
@@ -1,8 +1,8 @@
 cask "polvo" do
-  version "0.3.0"
-  sha256 "fbc0170fec9b2f2fc9ce052defccd7ba744b5200fe924d9a2e6eb3b6318bc386"
+  version "0.3.1"
+  sha256 "2c84a593ff74a0bb7b1e1c78ccbfab52a4d67eacbc8fc557f8b458c51f501642"
 
-  url "https://github.com/co2-lab/Polvo/releases/download/v0.3.0/Polvo_0.2.0_universal.dmg"
+  url "https://github.com/co2-lab/Polvo/releases/download/v0.3.1/Polvo_0.2.0_universal.dmg"
   name "Polvo"
   desc "AI agent orchestrator for spec-first projects"
   homepage "https://github.com/co2-lab/polvo"
